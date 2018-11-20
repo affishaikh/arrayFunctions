2b76f9bce837981242af69cfb0cb21e3cf5083a2
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-11-20 11:20:15 +0530

    New tests are added to test filter for against a function isGreaterThan10.

--- arrayFunctionsTest.js
+++ arrayFunctionsTest.js
@@ -9,7 +9,14 @@ const isEven = function(number) {
   return !(number%2);
 }
 
+const isNumberGreaterThan10 = function(number) {
+  return (number>10);
+}
+
 filterTest(isEven, [1], []);
 filterTest(isEven, [0], [0]);
 filterTest(isEven, [1, 2], [2]);
+filterTest(isNumberGreaterThan10, [1, 2], []);
+filterTest(isNumberGreaterThan10, [11], [11]);
+filterTest(isNumberGreaterThan10, [20, 2, 30], [20, 30]);
 console.log("All tests passed...");
